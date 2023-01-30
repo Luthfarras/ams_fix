1159fd7e73b243f83fcbdfa626fdc5627bc67faf
Luthfarras <luthfarras_tresanizega_27rpl@student.smktelkom-mlg.sch.id>
2023-01-30 22:29:31 +0700

    aa

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -29,8 +29,11 @@ class PenjualanController extends Controller
         // Menjumlahkan tabel penjualan pada kolom harga jumlah
         $jumlah = DB::table('penjualans')->select('jumlah')->sum('jumlah');
 
+        // DB::table('penjualans')->where(DB::raw(YEAR('tanggal_kirim')), $year);
+        $tahuns = $penjualan->unique('tanggal_kirim');
+
         // Masuk ke halaman home dengan membawa data yang sudah dideklarasikan
-        return view('menu.penjualan', compact('penjualan', 'profil', 'jumlah'));
+        return view('menu.penjualan', compact('penjualan', 'profil', 'jumlah', 'tahuns'));
     }
 
     /**
@@ -129,7 +132,7 @@ class PenjualanController extends Controller
         return redirect('penjualan');
     }
 
-    public function printPenjualan()
+    public function printPenjualan(Request $request)
     {
         // Mengambil seluruh data yang ada dalam tabel Penjualan
         $penjualan = Penjualan::all();
@@ -165,8 +168,8 @@ class PenjualanController extends Controller
         'jumlah' => $jumlah, 
         'result' => $result,
     ]);
-        
+        dd($tahuns);
         // PDF akan ditampilkan secara stream dengan ukuran A4-Landscape dan bisa didownload dengan nama yang sudah dideklarasikan
-        return $pdf->setPaper('a4', 'landscape')->stream('Data Penjualan - '. Carbon::now(). '.pdf');
+        // return $pdf->setPaper('a4', 'landscape')->stream('Data Penjualan - '. Carbon::now(). '.pdf');
     }
 }
